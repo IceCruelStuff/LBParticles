460d54cce8561cba683f809b22266ff921f848c9
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-10-25 21:32:28 -0700

    Update Main.php

--- src/LBParticles/Main.php
+++ src/LBParticles/Main.php
@@ -87,7 +87,7 @@ class Main extends PluginBase implements Listener {
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
         $subcommand = strtolower(array_shift($args));
         switch ($subcommand) {
-            case "give";
+            case "give":
                 if (count($args) < 1) {
                     array_unshift($args, $sender->getDisplayName());
                 }
